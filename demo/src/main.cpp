9c825fb5dca8ec195096fd428b03984444606737
Vo Nguyen Giap <vonguyengiap151103@gmail.com>
2024-11-30 00:07:17 +0700

    Bai 5 - Read analog input

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -1,33 +1,19 @@
 #include <Arduino.h>
 
-#define LED 16
+#define INPUT_PIN 34
 
-const int freq = 60;
-const int resolution = 10; // 0-16 bit
-const int channel = 0;
-/*CHANNEL: each channel has 6 GPIO port
-0: 16-17-18-19-21-22
-1: 0-4-5-12-13-14
-2: 2-12-13-14-15-16
-3: ...*/
+int value;
 
 void setup()
 {
-  ledcSetup(channel, freq, resolution);
-  ledcAttachPin(LED, channel);
+  Serial.begin(9600);
+  delay(200);
 }
 
 void loop()
 {
-  for (int i = 0; i <= 255; i++)
-  {
-    ledcWrite(channel, i);
-    delay(20);
-  }
-
-  for (int i = 255; i >= 0; i--)
-  {
-    ledcWrite(channel, i);
-    delay(20);
-  }
+  value = analogRead(INPUT_PIN);
+  Serial.print("Value: ");
+  Serial.println(value);
+  delay(500);
 }
